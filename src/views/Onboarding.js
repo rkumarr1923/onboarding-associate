5da01ded1ee2f87df8d29893aed578d18c768306
Supriyawinisri1012 <shanmr17@in.ibm.com>
2022-08-10 18:04:51 +0530

    CheckList and Comment Comments added

--- src/views/Onboarding.js
+++ src/views/Onboarding.js
@@ -1,43 +1,48 @@
-import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import { Button } from '../components/core';
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import { Button } from "../components/core";
 
 export default function Onboarding() {
   return (
-    <div className='onboarding-wrapper'>
-      <ul className='nav-links'>
+    <div className="onboarding-wrapper">
+      <ul className="nav-links">
         <li>
           <Link to="/newAssociate">
-            <Button label='Add New Associate' />
+            <Button label="Add New Associate" />
           </Link>
         </li>
         <li>
           <Link to="/dateFilter">
-            <Button label='Date Filter' />
+            <Button label="Date Filter" />
           </Link>
         </li>
         <li>
           <Link to="/allAssociates">
-            <Button label='All Associates' />
+            <Button label="All Associates" />
           </Link>
         </li>
         <li>
           <Link to="/onBoardingCheckList">
-            <Button label='On-boarding Checklist' />
+            <Button label="On-boarding Checklist" />
           </Link>
         </li>
         <li>
           <Link to="/uploadDocuments">
-            <Button label='Upload Documents' />
+            <Button label="Upload Documents" />
           </Link>
         </li>
         <li>
           <Link to="/trainingLinks">
-            <Button label='Taining Links' />
+            <Button label="Taining Links" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/comment">
+            <Button label="Comment" />
           </Link>
         </li>
       </ul>
-      <div className='onboarding-container'>
+      <div className="onboarding-container">
         <Outlet />
       </div>
     </div>
