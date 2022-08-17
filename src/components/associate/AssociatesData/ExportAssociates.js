1c5a500d75155b4c6e2d6e17a093a106e4145ebc
Vijay Gupta07 <vigupt07@in.ibm.com>
2022-08-17 21:13:20 +0530

    Export Associate Data into Excel Changes

--- src/components/associate/AssociatesData/ExportAssociates.js
+++ src/components/associate/AssociatesData/ExportAssociates.js
@@ -1,114 +1,98 @@
 import React from "react";
+import Excel from 'exceljs';
+import { saveAs } from 'file-saver';
+import { allAssociates } from "../DatePicker/all-associates-data";
 import '../../styles/associate.css';
-// import axios from "axios";
-// import FileSaver from "file-saver";
-// import XLSX from "xlsx";
 
-const EXCEL_TYPE =
-  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
-const EXCEL_EXTENSION = ".xlsx";
+
+const columns = [
+  { header: 'Associate Name', key: 'associateName' },
+  { header: 'IBM Emp Id', key: 'ibmId' },
+  { header: 'IBM Email', key: 'emailIBM' },
+  { header: 'Location', key: 'location' },
+  { header: 'Role', key: 'role' },
+  { header: 'Primary Contact', key: 'primaryContact' },
+  { header: 'IT Experience Date', key: 'itExpDate' }
+];
+
+const workSheetName = 'Associates-Details';
+const workBookName = 'Associates';
 
 const ExportAssociates = ({ }) => {
 
-//   const [excelData, setIExcelData] = React.useState([]);
+  const workbook = new Excel.Workbook();
 
-//   const exportIntoExcel = () => {
-//     console.log("called function to export data into excel");
-//     // axios.get("https://api.thedogapi.com/v1/breeds?limit=10&page=0").then((response) => {
-//     //     setIExcelData(response.data);
-//     // });
-//     setIExcelData(props.inputExcelData);
-//     console.log("excel data ", props.inputExcelData);
-//     if (props.inputExcelData?.checkListDetails) {
-//       let checkListExceldata = props.inputExcelData.checkListDetails.map(
-//         (data) => {
-//           let updatedData = {
-//             Question: data.questions,
-//             Date: data.date,
-//             Status: data.status,
-//             Comment: data.comment,
-//           };
-//           return updatedData;
-//         }
-//       );
-//       exportAsExcelFile(checkListExceldata, "CheckList");
-//     } else exportAsExcelFile(props.inputExcelData, "AllAssociates");
-//   };
+  const exportIntoExcel = async () => {
+    try {
 
-//   const exportAsExcelFile = (json, excelFileName) => {
-//     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
-//     var wscols = [
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//       { wch: 30 },
-//     ];
+      const fileName = workBookName;
 
-//     worksheet["!cols"] = wscols;
-//     const workbook: XLSX.WorkBook = {
-//       Sheets: { data: worksheet },
-//       SheetNames: ["data"],
-//     };
-//     const excelBuffer: any = XLSX.write(workbook, {
-//       bookType: "xlsx",
-//       type: "array",
-//     });
-//     saveAsExcelFile(excelBuffer, excelFileName);
-//   };
+      // creating one worksheet in workbook
+      const worksheet = workbook.addWorksheet(workSheetName);
 
-//   const saveAsExcelFile = (buffer, fileName) => {
-//     const data: Blob = new Blob([buffer], {
-//       type: EXCEL_TYPE,
-//     });
-//     FileSaver.saveAs(
-//       data,
-//       fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
-//     );
-//   };
+      // add worksheet columns
+      // each columns contains header and its mapping key from data
+      worksheet.columns = columns;
 
-  return (
-    <>
-      <div className="download-icon">
-        <i
-          className="fa fa-download"
-        //   onClick={() => exportIntoExcel()}
-          aria-hidden="true"
-        ></i>
-      </div>
-    </>
-  );
-};
+      // updated the font for first row.
+      worksheet.getRow(1).font = { bold: true };
 
-export default ExportAssociates;
+      // loop through all of the columns and set the alignment with width.
+      worksheet.columns.forEach(column => {
+        column.width = column.header.length + 5;
+        column.alignment = { horizontal: 'center' };
+      });
+
+      // loop through data and add each one to worksheet
+      allAssociates.forEach(singleData => {
+        worksheet.addRow(singleData);
+      });
+
+      // loop through all of the rows and set the outline style.
+      worksheet.eachRow({ includeEmpty: false }, row => {
+        // store each cell to currentCell
+        const currentCell = row._cells;
+
+        // loop through currentCell to apply border only for the non-empty cell of excel
+        currentCell.forEach(singleCell => {
+          // store the cell address i.e. A1, A2, A3, B1, B2, B3, ...
+          const cellAddress = singleCell._address;
+
+          // apply border
+          worksheet.getCell(cellAddress).border = {
+            top: { style: 'thin' },
+            left: { style: 'thin' },
+            bottom: { style: 'thin' },
+            right: { style: 'thin' }
+          };
+        });
+      });
+
+      // write the content using writeBuffer
+      const buf = await workbook.xlsx.writeBuffer();
+
+      // download the processed file
+      saveAs(new Blob([buf]), `${fileName}.xlsx`);
+    } catch (error) {
+      console.error('<<<ERRROR>>>', error);
+      console.error('Something Went Wrong', error.message);
+    } finally {
+      // removing worksheet's instance to create new one
+      workbook.removeWorksheet(workSheetName);
+    }
+  };
+
+    return (
+      <>
+        <div className="download-icon">
+          <i
+            className="fa fa-download"
+            onClick={() => exportIntoExcel()}
+            aria-hidden="true"
+          ></i>
+        </div>
+      </>
+    );
+  };
+
+  export default ExportAssociates;
