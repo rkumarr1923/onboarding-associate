383c0638c65d95425a37133d150d5dfdc5ab4225
Shruthi Sreekumar <>
2022-08-23 16:17:16 +0530

    Update UploadDocument.js
    Refactored Fetch and delete logic

--- src/components/document/UploadDocument.js
+++ src/components/document/UploadDocument.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 import './UploadDocument.css';
 import Alert from '@mui/material/Alert';
 
-export default class UploadDeleteDocComponent extends React.Component {
+const UploadDocument = () => {
 
-    fileUploaderRef;
+    const [documents, setDocuments] = useState([]);
+    //const [state, setState] = useState([]);
+    useEffect(() => {
+        fetchDocuments()
+    }, []);
 
+<<<<<<< Updated upstream
     constructor(props) {
         super(props);
         this.state = {
@@ -107,14 +112,26 @@ export default class UploadDeleteDocComponent extends React.Component {
     callUploadAPI(file,idx) {
         //var input = document.getElementById('file');
         //console.log('inputFileElement.value', input.files[0]);
+=======
+    const callUploadAPI = () => {
+
+        var input = document.getElementById('myfile');
+        console.log('inputFileElement.value', input.files[0]);
+>>>>>>> Stashed changes
         var formdata = new FormData();
         formdata.append("file", file.fileName,file.fileName.name );
 
+        var requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        };
         axios.post("http://localhost:9003/uploadFile", formdata, {
             headers: {
                 "Content-Type": "multipart/form-data"
             }
         })
+<<<<<<< Updated upstream
             .then(result => {
                 if(result){
                     this.state.files[idx].Status="Success";
@@ -178,5 +195,70 @@ export default class UploadDeleteDocComponent extends React.Component {
                 
             </div>
         );
+=======
+            .then(result => console.log(result))
+
     }
+
+    const fetchDocuments = () => {
+        axios
+            .get('http://localhost:9003/files')
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);
+                setDocuments(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    const deleteDocs = (id) => {
+        axios.delete(`http://localhost:9003/files/delete/${id}`)
+             .then(result => {
+                console.log(result)
+                fetchDocuments()
+             })
+             .catch((err) => {
+                console.log(err);
+            });
+        //  fetch(`http://localhost:9003/delete/${name}`, { method: 'DELETE', mode: 'no-cors' })
+        //  .then(result => console.log(result));
+>>>>>>> Stashed changes
+    }
+
+
+    const fileUpload = (event) => {
+        //let changedFile = event.target.files[0];
+        let uploadedFiles = event.target.files;
+    }
+
+    return (
+        <div className='mar-left-10'>
+            <h2>Upload Documents</h2>
+            <div className='file-upload-wrapper' data-text="Select your file!">
+
+                <input type="file" id="myfile" name="myfile" onChange={fileUpload} multiple />
+                <button onClick={() => callUploadAPI()}>Upload</button>
+
+            </div>
+            <h3 className='bold'>Uploaded Documents:</h3>
+            <table className="filesName">
+                <tbody>
+                    {
+                        documents.map((doc, i) => {
+                            return <tr key={i}>
+                                <td style={{ textAlign: "left" }}>{doc.name} :</td>
+                                <td>
+                                    <button onClick={() => deleteDocs(doc.id)}>Delete</button>
+                                </td>
+                            </tr>
+                        })}
+                </tbody>
+            </table>
+
+        </div>
+
+    )
 }
+export default UploadDocument
