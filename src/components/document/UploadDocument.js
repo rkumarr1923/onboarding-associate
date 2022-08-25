7576ef88a166591f6a9da8c9deddd50a525d98a4
vasanth-admin-at-014388577598 <vasanth@example.com>
2022-08-25 11:14:20 +0530

    Added material ui dependency, added table for displaying uploaded documents

--- src/components/document/UploadDocument.js
+++ src/components/document/UploadDocument.js
@@ -1,98 +1,167 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
-import './UploadDocument.css';
+import React, { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import axios from "axios";
+import "./UploadDocument.css";
+import { Typography } from "@mui/material";
 
 const UploadDocument = () => {
+  const [documents, setDocuments] = useState([]);
+  const [open, setDialogStatus] = useState(false);
+  const [docTobeDeleted, setDocIdTobeDeleted] = useState({});
+  //const [state, setState] = useState([]);
+  useEffect(() => {
+    fetchDocuments();
+  }, []);
 
-    const [documents, setDocuments] = useState([]);
-    //const [state, setState] = useState([]);
-    useEffect(() => {
-        fetchDocuments()
-    }, []);
+  const callUploadAPI = () => {
+    var input = document.getElementById("myfile");
+    console.log("inputFileElement.value", input.files[0]);
+    var formdata = new FormData();
+    formdata.append("file", input.files[0], input.files[0].name);
 
-    const callUploadAPI = () => {
-
-        var input = document.getElementById('myfile');
-        console.log('inputFileElement.value', input.files[0]);
-        var formdata = new FormData();
-        formdata.append("file", input.files[0], input.files[0].name);
-
-        var requestOptions = {
-            method: 'POST',
-            body: formdata,
-            redirect: 'follow'
-        };
-        axios.post("http://localhost:9003/files", formdata, {
-            headers: {
-                "Content-Type": "multipart/form-data"
-            }
-        })
-            .then(result => {
-                document.getElementById('myfile').value = '';
-                fetchDocuments();
-                console.log(result)
-            })
-
-    }
-
-    const fetchDocuments = () => {
-        axios
-            .get('http://localhost:9003/files')
-            .then((res) => {
-                console.log(res);
-                console.log(res.data);
-                setDocuments(res.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+    var requestOptions = {
+      method: "POST",
+      body: formdata,
+      redirect: "follow",
     };
+    axios
+      .post("http://localhost:9003/files", formdata, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((result) => {
+        document.getElementById("myfile").value = "";
+        fetchDocuments();
+        console.log(result);
+      });
+  };
 
-    const deleteDocs = (id) => {
-        axios.delete(`http://localhost:9003/files/delete/${id}`)
-             .then(result => {
-                console.log(result)
-                fetchDocuments()
-             })
-             .catch((err) => {
-                console.log(err);
-            });
-        //  fetch(`http://localhost:9003/delete/${name}`, { method: 'DELETE', mode: 'no-cors' })
-        //  .then(result => console.log(result));
-    }
+  const fetchDocuments = () => {
+    axios
+      .get("http://localhost:9003/files")
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        setDocuments(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
+  const openDialog = (doc) => {
+    setDocIdTobeDeleted(doc);
+    setDialogStatus(true);
+  };
 
-    const fileUpload = (event) => {
-        //let changedFile = event.target.files[0];
-        let uploadedFiles = event.target.files;
-    }
+  const deleteDocs = (id) => {
+    axios
+      .delete(`http://localhost:9003/files/delete/${id}`)
+      .then((result) => {
+        setDialogStatus(false);
+        console.log(result);
+        fetchDocuments();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    //  fetch(`http://localhost:9003/delete/${name}`, { method: 'DELETE', mode: 'no-cors' })
+    //  .then(result => console.log(result));
+  };
 
-    return (
-        <div className='mar-left-10'>
-            <h2>Upload Documents</h2>
-            <div className='file-upload-wrapper' data-text="Select your file!">
+  const fileUpload = (event) => {
+    //let changedFile = event.target.files[0];
+    let uploadedFiles = event.target.files;
+  };
 
-                <input type="file" id="myfile" name="myfile" onChange={fileUpload} multiple />
-                <button onClick={() => callUploadAPI()}>Upload</button>
+  const handleClose = () => {
+    setDialogStatus(false);
+  };
 
-            </div>
-            <h3 className='bold'>Uploaded Documents:</h3>
-            <table className="filesName">
-                <tbody>
-                    {
-                        documents.map((doc, i) => {
-                            return <tr key={i}>
-                                <td style={{ textAlign: "left" }}>{doc.name} :</td>
-                                <td>
-                                    <button onClick={() => deleteDocs(doc.id)}>Delete</button>
-                                </td>
-                            </tr>
-                        })}
-                </tbody>
-            </table>
-
-        </div>
-
-    )
-}
-export default UploadDocument
+  return (
+    <div className="mar-left-10">
+      <h2>Upload Documents</h2>
+      <div className="file-upload-wrapper" data-text="Select your file!">
+        <input
+          type="file"
+          id="myfile"
+          name="myfile"
+          onChange={fileUpload}
+          multiple
+        />
+        <button onClick={() => callUploadAPI()}>Upload</button>
+      </div>
+      <h3 className="bold">Uploaded Documents:</h3>
+      {documents.length > 0 ? (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>S.No.</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {documents.map((doc, i) => (
+                <TableRow
+                  key={i}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {i + 1}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {doc.name}
+                  </TableCell>
+                  <TableCell>
+                    <Button color="secondary" onClick={() => openDialog(doc)}>
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <Typography>No Records Exist</Typography>
+      )}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {`Are you sure you want to delete the document ${docTobeDeleted.name}?`}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Once deleted canot be reverted.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={() => deleteDocs(docTobeDeleted.id)} autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+export default UploadDocument;
