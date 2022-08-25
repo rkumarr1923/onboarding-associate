6e790102a2332ad536aeb10c461b111779a5ddb8
Vijay Gupta07 <vigupt07@in.ibm.com>
2022-08-25 12:07:39 +0530

    added dynamic styling to status cell based to associate status

--- src/components/associate/AllAssociates.js
+++ src/components/associate/AllAssociates.js
@@ -10,32 +10,9 @@ import '../styles/associate.css';
 
 const AllAssociates = () => {
 
-    const [gridColumnApi, setGridColumnApi] = useState([]);
     const [serviceData, setServiceData] = useState([]);
     const [gridApi, setGridApi] = useState([]);
     const [tableValues] = useState(allAssociates);
-    const [dataSet] = useState([
-        { id: 1, eventTimestamp: '2020-10-25T04:34:21Z' },
-        { id: 2, eventTimestamp: '2020-04-23T20:21:55Z' },
-        { id: 3, eventTimestamp: '2020-05-01T16:16:38Z' },
-        { id: 4, eventTimestamp: '2020-11-24T22:10:03Z' },
-        { id: 5, eventTimestamp: '2020-08-10T13:52:03Z' },
-        { id: 6, eventTimestamp: '2020-10-18T18:49:19Z' },
-        { id: 7, eventTimestamp: '2020-11-30T00:51:26Z' },
-        { id: 8, eventTimestamp: '2020-03-28T13:30:51Z' },
-        { id: 9, eventTimestamp: '2020-12-18T12:39:26Z' },
-        { id: 10, eventTimestamp: '2020-03-13T18:39:02Z' },
-        { id: 11, eventTimestamp: '2021-02-13T15:03:47Z' },
-        { id: 12, eventTimestamp: '2020-03-11T14:03:56Z' },
-        { id: 13, eventTimestamp: '2021-01-14T06:38:59Z' },
-        { id: 14, eventTimestamp: '2020-08-17T06:50:21Z' },
-        { id: 15, eventTimestamp: '2020-04-03T18:34:12Z' },
-        { id: 16, eventTimestamp: '2020-08-08T19:21:06Z' },
-        { id: 17, eventTimestamp: '2020-10-25T18:24:39Z' },
-        { id: 18, eventTimestamp: '2020-03-12T14:45:03Z' },
-        { id: 19, eventTimestamp: '2021-01-11T12:45:54Z' },
-        { id: 20, eventTimestamp: '2020-03-22T09:37:58Z' },
-    ]);
 
     useEffect(() => {
         axios
@@ -149,6 +126,7 @@ const AllAssociates = () => {
             field: "status",
             headerName: "Status",
             cellStyle: { textAlign: '' },
+            cellClass: params => onBoardStatus(params.data.status),
             minWidth: 100,
             maxWidth: 175
         },
@@ -156,7 +134,6 @@ const AllAssociates = () => {
 
     const onGridReady = (params) => {
         setGridApi(params.api);
-        setGridColumnApi(params.columnApi);
         params.api.addGlobalListener((type, event) => {
             switch (type) {
                 case "filterChanged":
@@ -167,6 +144,27 @@ const AllAssociates = () => {
             }
         });
     };
+
+    const onBoardStatus = (status) => {
+        switch (status) {
+            case 'Yet to be started':
+                return 'associate-yet-to-be-started';
+            case 'In progress':
+                return 'associate-in-progress';
+            case 'Under review':
+                return 'associate-under-review';
+            case 'Back in progress':
+                return 'associate-back-in-progress';
+            case 'Completed':
+                return 'associate-completed';
+            case 'Waiting':
+                return 'associate-waiting';
+            case 'Blocked':
+                return 'associate-blocked';
+            default:
+                return 'associate-in-progress';
+        }
+    }
 
     return (
         <>
