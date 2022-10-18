9acd893c3937822a14e1b528e577c5c291bfbd20
Supriyawinisri1012 <shanmr17@in.ibm.com>
2022-10-18 14:28:54 +0530

    Training service updates

--- src/components/associate-useful/TrainingLinks/TrainingMainComponent.jsx
+++ src/components/associate-useful/TrainingLinks/TrainingMainComponent.jsx
@@ -2,102 +2,115 @@ import React, { useState, useEffect } from 'react';
 import TrainingList from './TrainingList';
 import AddTraining from './AddTraining';
 import EditTraining from './EditTraining';
-import { Grid } from "@mui/material";
-import axios from "axios";
-import TrainingService from "../../../services/hooks/TrainingService";
-
+import { Grid } from '@mui/material';
+import axios from 'axios';
+import TrainingService from '../../../services/hooks/TrainingService';
+import { useSelector } from 'react-redux';
+import { token } from '../../../store';
 
 const TrainingMainComponent = () => {
+  const userToken = useSelector(token);
+  const initialFormState = {
+    trainingId: '',
+    trainingName: '',
+    link: '',
+    remarks: '',
+  };
+  const [trainings, setTrainings] = useState([]);
+  const BASE_URL = 'http://localhost:9094/training';
+  useEffect(() => {
+    const BASE_URL = 'http://localhost:9094/training';
 
-    const initialFormState = { trainingId: '', trainingName: '', link: '', remarks: '' };
-    const [trainings, setTrainings] = useState([]);
-    const BASE_URL = "http://localhost:9094/training";
-    useEffect(() => {
-        const BASE_URL = "http://localhost:9094/training";
-        
-        axios.get(BASE_URL + '/get-all-training')
-        .then((response) => {
-            setTrainings(response.data);
-            console.log("in Main component useEffect getall trainings ",response.data);
-        })
-        //retrieveTrainings();
-      },[]);
-    //  const retrieveTrainings = () => {
-    //      const response = axios.get(BASE_URL + '/get-all-training');
-    //      setTrainings(response.data);
-    //      console.log("in main component, getall trainings ",response.data);
-    //  };
-    
-    const [editing, setEditing] = useState(false);
-    const [currentTraining, setCurrentTraining] = useState(initialFormState)
-    // Add training...
-    const addTraining = training => {
-        console.log("in main component, add training", training);
-        setTrainings([...trainings, training]);
-    }
-    // delete trainings...
-    const deleteTraining = trainingId => {
-        TrainingService.deleteTraining(trainingId)
-        .then(response => {
-            console.log(response.data);
-            setTrainings(trainings.filter(training => training.trainingId !== trainingId))
-        })
-    }
-    // set value for edit training form...
-    const editTraining = (training) => {
-        setEditing(true)
-        setCurrentTraining({
-            trainingId: training.trainingId,
-            trainingName: training.trainingName,
-            link: training.link,
-            remarks: training.remarks
-        })
-    }
-    //  update training
-    const updateTraining = (trainingId, updatedTraining) => {
-        console.log("in updateTraining", trainingId);
-        setEditing(false);
-        setTrainings(
-            trainings.map(item => {
-                return (item.trainingId === trainingId ? updatedTraining : item)
-            }));
-    }
+    axios
+      .get(BASE_URL + '/get-all-training', {
+        headers: { Authorization: 'Bearer ' + userToken },
+      })
+      .then((response) => {
+        setTrainings(response.data);
+        console.log(
+          'in Main component useEffect getall trainings ',
+          response.data
+        );
+      });
+    //retrieveTrainings();
+  }, []);
+  //  const retrieveTrainings = () => {
+  //      const response = axios.get(BASE_URL + '/get-all-training');
+  //      setTrainings(response.data);
+  //      console.log("in main component, getall trainings ",response.data);
+  //  };
 
-    return (
-        <Grid container >
-            <Grid item xs={12}>
-                {editing ? (
-                    <Grid item xs={12}>
-                        <h2 style={{textAlign:"center"}}>Edit Training</h2>
-                        <Grid item xs={12}>
-                            <EditTraining
-                                editing={editing}
-                                setEditing={setEditing}
-                                currentTraining={currentTraining}
-                                updateTraining={updateTraining}
-                            />
-                        </Grid>
-                    </Grid>
-                ) : (
-                    <Grid item xs={12}>
-                        <h2 style={{textAlign:"center"}}>Add Training</h2>
-                        <Grid item xs={12}>
-                            <AddTraining addTraining={addTraining} />
-                        </Grid>
-                    </Grid>
-                )}
-
-                
-                <Grid item xs={12}>
-                        <TrainingList trainings={trainings} editTraining={editTraining} deleteTraining={deleteTraining} />
-                </Grid>
-                
-            </Grid>
-        </Grid>
+  const [editing, setEditing] = useState(false);
+  const [currentTraining, setCurrentTraining] = useState(initialFormState);
+  // Add training...
+  const addTraining = (training) => {
+    console.log('in main component, add training', training);
+    setTrainings([...trainings, training]);
+  };
+  // delete trainings...
+  const deleteTraining = (trainingId) => {
+    TrainingService.deleteTraining(trainingId).then((response) => {
+      console.log(response.data);
+      setTrainings(
+        trainings.filter((training) => training.trainingId !== trainingId)
+      );
+    });
+  };
+  // set value for edit training form...
+  const editTraining = (training) => {
+    setEditing(true);
+    setCurrentTraining({
+      trainingId: training.trainingId,
+      trainingName: training.trainingName,
+      link: training.link,
+      remarks: training.remarks,
+    });
+  };
+  //  update training
+  const updateTraining = (trainingId, updatedTraining) => {
+    console.log('in updateTraining', trainingId);
+    setEditing(false);
+    setTrainings(
+      trainings.map((item) => {
+        return item.trainingId === trainingId ? updatedTraining : item;
+      })
     );
+  };
 
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        {editing ? (
+          <Grid item xs={12}>
+            <h2 style={{ textAlign: 'center' }}>Edit Training</h2>
+            <Grid item xs={12}>
+              <EditTraining
+                editing={editing}
+                setEditing={setEditing}
+                currentTraining={currentTraining}
+                updateTraining={updateTraining}
+              />
+            </Grid>
+          </Grid>
+        ) : (
+          <Grid item xs={12}>
+            <h2 style={{ textAlign: 'center' }}>Add Training</h2>
+            <Grid item xs={12}>
+              <AddTraining addTraining={addTraining} />
+            </Grid>
+          </Grid>
+        )}
 
-}
-
+        <Grid item xs={12}>
+          <TrainingList
+            trainings={trainings}
+            editTraining={editTraining}
+            deleteTraining={deleteTraining}
+          />
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default TrainingMainComponent;
