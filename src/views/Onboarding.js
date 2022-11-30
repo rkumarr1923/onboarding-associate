ef8ec85988771b46ae2b3427773320983c97eb71
AratiPatilNare <68734108+AratiPatilNare@users.noreply.github.com>
2022-11-30 15:18:01 +0530

    Previous & Next Button

--- src/views/Onboarding.js
+++ src/views/Onboarding.js
@@ -1,115 +1,139 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, Outlet } from 'react-router-dom';
-import { Button } from '../components/core';
-import { tabSelected, appStore } from '../store';
+import { useSelector, useDispatch } from "react-redux";
+import { Link, Outlet } from "react-router-dom";
+import { tabSelected, appStore } from "../store";
+//import { Tab, Tabs } from "@material-ui/core";
+import { Route} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Tabs, Tab, Row, Button, Stack, Col, Container } from "react-bootstrap";
+//import "./styles.css";
 
 export default function Onboarding() {
   const dispatch = useDispatch();
   const store = useSelector(appStore);
   const { activeTab, userDetails: user } = store || {};
-  const isTabActive = (label) => (activeTab === label ? 'active-tab' : '');
-  const tabClicked = (tab) => dispatch(tabSelected({ tab }));
-
+  const [currentTab, setCurrentTab] = React.useState(0); 
+ 
+  const routes = ["auth/register","/allAssociates","/trainingLinks","/comment","/recording","/sampleDocuments","/bgCheck"]
   const loginFormRender = () => {
     return (
       <ul className="nav-links">
         {user &&
           (user.role === 'ROLE_ONBOARDING_REVIEWER' ||
             user.role === 'ROLE_ONBOARDING_MANAGER') && (
-            <>
-              <li className={isTabActive('New User')}>
-                <Link to="/auth/register">
-                  <Button
-                    label="New User"
-                    clickHandler={() => tabClicked('New User')}
-                  />
-                </Link>
-              </li>
-              <li className={isTabActive('All Associates')}>
-                <Link to="/allAssociates">
-                  <Button
-                    label="All Associates"
-                    clickHandler={() => tabClicked('All Associates')}
-                  />
-                </Link>
-              </li>
-              <li className={isTabActive('Training Links')}>
-                <Link to="/trainingLinks">
-                  <Button
-                    label="Training Links"
-                    clickHandler={() => tabClicked('Training Links')}
-                  />
-                </Link>
-              </li>
-              <li className={isTabActive('Recordings')}>
-                <Link to="/recording">
-                  <Button
-                    label="Recordings"
-                    clickHandler={() => tabClicked('Recordings')}
-                  />
-                </Link>
-              </li>
-              <li className={isTabActive('Sample Documents')}>
-                <Link to="/sampleDocuments">
-                  <Button
-                    label="Sample Documents"
-                    clickHandler={() => tabClicked('Sample Documents')}
-                  />
-                </Link>
-              </li>
-              <li className={isTabActive('BackGround Check')}>
-                <Link to="/bgCheck">
-                  <Button
-                    label="BackGround Check"
-                    clickHandler={() => tabClicked('BackGround Check')}
-                  />
-                </Link>
-              </li>
-            </>
+ 	    <>
+         
+         <Tabs  variant='scrollable'>
+ 
+                <Tab
+                  value={routes[0]}
+                  eventKey={0}
+                  label="New User"
+                  component={Link}
+                  to={routes[0]}
+                  disabled={currentTab !== 0}
+                />
+
+              <Tab
+                  value={routes[1]}
+                  label="All Associates"
+                  component={Link}
+                  to={routes[1]}
+                />
+              <Tab
+                  value={routes[2]}
+                  label="Training Links"
+                  component={Link}
+                  to={routes[2]}
+                />
+              <Tab
+                  value={routes[3]}
+                  label="Comment"
+                  component={Link}
+                  to={routes[3]}
+                />
+              <Tab
+                  value={routes[4]}
+                  label="Recordings"
+                  component={Link}
+                  to={routes[4]}
+                />
+              <Tab
+                  value={routes[5]}
+                  label="Sample Documents"
+                  component={Link}
+                  to={routes[5]}
+                />
+              <Tab
+                  value={routes[6]}
+                  label="Background Check"
+                  component={Link}
+                  to={routes[6]}
+                />
+          </Tabs>
+         
+	
+          <Stack gap={6} direction="horizontal" className="mt-3">
+        <Button
+          className="success"
+          disabled={currentTab === 0}
+          onClick={() => setCurrentTab((prev) => prev - 1)}
+        >
+          Prev
+        </Button>
+        <Button
+          className="success"
+          disabled={currentTab === 6}
+          onClick={() => setCurrentTab((prev) => prev + 1)}
+        >
+          Next
+        </Button>
+      </Stack>
+      <p>Current tab index is {currentTab}</p>
+        
+      </>
           )}
         {user && user.role === 'ROLE_ASSOCIATE' && (
           <>
-            <li className={isTabActive('On-boarding Checklist')}>
-              <Link to="/onBoardingCheckList">
-                <Button
-                  label="On-boarding Checklist"
-                  clickHandler={() => tabClicked('On-boarding Checklist')}
+            
+                 <Tab
+                  value={routes[2]}
+                  label="OnBoardingCheckList"
+                  component={Link}
+                  to={routes[2]}
                 />
-              </Link>
-            </li>
-            <li className={isTabActive('Sample Documents')}>
-              <Link to="/sampleDocuments">
-                <Button
-                  label="Sample Documents"
-                  clickHandler={() => tabClicked('Sample Documents')}
+              
+              <Tab
+                  value={routes[3]}
+                  label="UploadDocument"
+                  component={Link}
+                  to={routes[3]}
                 />
-              </Link>
-            </li>
-            <li className={isTabActive('Upload Documents')}>
-              <Link to="/uploadDocuments" state={{ forAssociate: {} }}>
-                <Button
-                  label="Upload Documents"
-                  clickHandler={() => tabClicked('Upload Documents')}
-                />
-              </Link>
-            </li>
-            <li className={isTabActive('Comment')}>
-              <Link to="/comment">
-                <Button
+             
+              <Tab
+                  value={routes[4]}
                   label="Comment"
-                  clickHandler={() => tabClicked('Comment')}
+                  component={Link}
+                  to={routes[4]}
                 />
-              </Link>
-            </li>
-            <li className={isTabActive('Recordings')}>
-              <Link to="/recording">
-                <Button
-                  label="Recordings"
-                  clickHandler={() => tabClicked('Recordings')}
-                />
-              </Link>
-            </li>
+             <Stack gap={3} direction="horizontal" className="mt-3">
+        <Button
+          className="success"
+          disabled={currentTab === 0}
+          onClick={() => setCurrentTab((prev) => prev - 1)}
+        >
+          Prev
+        </Button>
+        <Button
+          className="success"
+          disabled={currentTab === 2}
+          onClick={() => setCurrentTab((prev) => prev + 1)}
+        >
+          Next
+        </Button>
+      </Stack>
+      <p>Current tab index is {currentTab}</p>
           </>
         )}
       </ul>
